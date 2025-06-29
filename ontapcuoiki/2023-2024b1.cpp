5512ed2df5c8d3dbf2fee3459fff2723b889231c
qhung25 <157773878+qhung25@users.noreply.github.com>
2025-06-29 20:37:22 +0700

    Add files via upload

--- ontapcuoiki/2023-2024b1.cpp
+++ ontapcuoiki/2023-2024b1.cpp
@@ -1,80 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct BST
+
+struct BinaryTreeNode 
 {
     int data;
-    BST* left;
-    BST* right;
+    BinaryTreeNode *left, *right;
 };
-void initTree(BST* &root)
-{
-    BST* r8 = new BST{13, nullptr, nullptr};
-    BST* r7 = new BST{14, r8, nullptr};
-    BST* r6 = new BST{10, nullptr, r7};
-    BST* r5 = new BST{7, nullptr, nullptr};
-    BST* r4 = new BST{4, nullptr, nullptr};
-    BST* r3 = new BST{6, r4, r5};
-    BST* r2 = new BST{1, nullptr, nullptr};
-    BST* r1 = new BST{3, r2, r3};
-    root = new BST{8, r1, r6};
 
-}
-void print(BST *root)
+void insert(BinaryTreeNode* &root, int x)
 {
-    if(root != nullptr){
-        cout << root->data << " " ;
-        print(root->left);
-        print(root->right);
-
+    if (root == nullptr)
+    {
+        root = new BinaryTreeNode();
+        root->data = x;
+        root->left = root->right = nullptr;
     }
-}
-// tinh tong so chan trong cay
-int sumChan(BST *root){
-    if(root == nullptr)
-        return 0;
     else
     {
-        int sum = 0;
-        if(root->data %2 == 0)
-            sum += root->data;
-        sum += sumChan(root->left);
-        sum += sumChan(root->right);
-        return sum;
-    }
-}
-// cap so x trong cay
-int capsoX(BST* root, int x, int h)
-{
-    if(root == nullptr)
-        return 0;
-    else
-    {
-        if(root->data == x)
-            return h;
-        int rootLeft = capsoX(root->left, x, h+1);
-        if(rootLeft != 0)
-            return rootLeft;
+        if (root->data > x)
+            insert(root->left, x);
         else
-        {
-            int rootRight = capsoX(root->right, x, h+1);
-            if(rootRight != 0)
-                return rootRight;
-            else
-                return 0;
-        }
+            insert(root->right, x);
     }
 }
 
-// câu d
-int main()
+int sumEven(BinaryTreeNode* root)
 {
-    BST* root = new BST();
-    initTree(root);
-
-    print(root);
-    cout << "Tong so chan trong cay: " << sumChan(root) << endl;
-    cout << "Cap so 6 trong cay: " << capsoX(root,88, 0) << endl;
+    if (root == nullptr) return 0;
+    int sum = 0;
+    if (root->data % 2 == 0) sum += root->data;
+    sum += sumEven(root->left);
+    sum += sumEven(root->right);
+    return sum;
 }
 
+int findLevel(BinaryTreeNode* root, int x, int level = 0)
+{
+    if (root == nullptr) return 0;
+    if (root->data == x) return level;
+    if (x < root->data)
+        return findLevel(root->left, x, level + 1);
+    else 
+        return findLevel(root->right, x, level + 1);
+}
 
-// Output: 2 
+int main() 
+{
+    BinaryTreeNode* root = nullptr;
+    int a[] = {8, 3, 10, 1, 6, 14, 4, 7, 13};
+    for (int i = 0; i < 9; i++)
+        insert(root, a[i]);
+    cout << "Sum of even numbers in the tree: " << sumEven(root) << endl;
+
+    cout << "Level of node with value 6 in the tree: " << findLevel(root, 6, 0) << endl;
+
+    return 0;
+}
